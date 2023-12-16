06e70ec79ced540af00dda09206c5f0672e27b6d
Diego Dy Suriaga <suriaga.diego@gmail.com>
2023-12-16 23:42:26 +0800

    landing page animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import './App.css'
-import Home from './modules/home.module'
+import "./App.css";
+import Home from "./modules/home.module";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navigation from './components/navigation';
-import Cart from './modules/cart.module';
-import Product from './modules/product.module';
+import Navigation from "./components/navigation";
+import Cart from "./modules/cart.module";
+import Product from "./modules/product.module";
+import Landing from "./modules/landing/landing.module";
 
 function App() {
-
   return (
     <div className="App">
       <Navigation />
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/cart" element={<Cart />}></Route>
-            <Route path="/product" element={<Product />}></Route>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/product" element={<Product />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
