ebd17f691b1793f506b2d96d91d349fb6920f977
Arriane E. Livara <arriane.livara@gmail.com>
2024-02-12 11:23:27 +0800

    add crud

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navigation from "./components/navigation";
 import Cart from "./modules/cart.module";
 import Product from "./modules/product.module";
 import Landing from "./modules/landing/landing.module";
+import ProductList from "./modules/product-list.module";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
           <Route path="/" element={<Landing />}></Route>
           <Route path="/home" element={<Home />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
-          <Route path="/product" element={<Product />}></Route>
+          <Route path="/product" element={<ProductList />}></Route>
+          <Route path="/product/:id" element={<Product />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/modules/product.module.jsx
+++ src/modules/product.module.jsx
@@ -1,10 +1,67 @@
 import React, { useState } from "react";
 import classnames from "classnames";
-import { Typography, Divider } from "antd";
+import { Typography, Divider, Skeleton, Button, Modal, Form, Input, notification } from "antd";
 const { Link, Text } = Typography;
+import useMount from "../hooks/useMount";
+import { getProduct, updateProduct, deleteProduct } from "../services/api/api";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Product = () => {
+  let { id } = useParams();
   const [selectedSize, setSelectedSize] = useState("");
+  const [productInfo, setProductInfo] = useState()
+  const [loading, setLoading] = useState(true)
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+  const navigate = useNavigate();
+  const fetchProduct = async (id) => {
+    const { data, error } = await getProduct(id)
+    setProductInfo(data);
+    setLoading(false);
+  }
+  
+
+  useMount(() => {
+    fetchProduct(id)
+  });
+
+  const handleButtonClick = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleDelete = () => {
+    setIsDeleteOpen(!isDeleteOpen);
+  };
+
+
+
+  const { product_name, description, price, uuid } = productInfo?.data || {}
+
+  const onFinish = async(values) => {
+    const body = {
+      product_name: values.productName,
+      description: values.descriptionField,
+      price: values.priceField
+    }
+    setIsFormOpen(false);
+    setLoading(true);
+    const { data, error } = await updateProduct(uuid, body)
+    fetchProduct(id)
+    setLoading(false);
+  };
+
+  const onDelete = async() => {
+    setIsDeleteOpen(false);
+    setLoading(true);
+    await deleteProduct(uuid);
+    notification.success({
+          message: 'Success',
+          description: 'Successfully deleted a product.',
+          duration: 3,
+      });
+    setLoading(false);
+    navigate('/product');
+  }
 
   return (
     <div className="flex justify-center mt-xl md:flex-row gap-4">
@@ -13,39 +70,55 @@ const Product = () => {
           <div className="flex flex-row gap-2">
             <div className="flex relative flex-col">
               <div className="border border-black-500 w-[75px] mb-sm rounded hover:opacity-75">
-                <img src="src/assets/images/shirt2.png" />
+                <img src="../src/assets/images/shirt2.png" />
               </div>
               <div className="border border-black-500 w-[75px] mb-sm rounded hover:opacity-75">
-                <img src="src/assets/images/shirt2.png" />
+                <img src="../src/assets/images/shirt2.png" />
               </div>
               <div className="border border-black-500 w-[75px] mb-sm rounded hover:opacity-75">
-                <img src="src/assets/images/shirt2.png" />
+                <img src="../src/assets/images/shirt2.png" />
               </div>
               <div className="border border-black-500 w-[75px] mb-sm rounded hover:opacity-75">
-                <img src="src/assets/images/shirt2.png" />
+                <img src="../src/assets/images/shirt2.png" />
               </div>
               <div className="border border-black-500 w-[75px] mb-sm rounded hover:opacity-75">
-                <img src="src/assets/images/shirt2.png" />
+                <img src="../src/assets/images/shirt2.png" />
               </div>
             </div>
             <div className="flex flex-col justify-center items-center px-md">
-              <img src="src/assets/images/shirt2.png" width={400} />
+              <img src="../src/assets/images/shirt2.png" width={400} />
             </div>
           </div>
         </div>
       </div>
 
       <div className="flex flex-col w-3/12">
-        <Text className="uppercase font-bold text-lg text-left">
-          Product Page
-        </Text>
-        <Text className="uppercase font-md text-sm text-left mt-md leading-normal">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam
-          explicabo itaque illum corporis. Quasi qui quidem expedita tenetur in
-          optio voluptatibus suscipit corporis, cumque cum dolorem doloribus
-          temporibus nisi pariatur!
-        </Text>
-        <Text className="font-bold text-lg text-left mt-sm">P800</Text>
+        {
+          loading ? <Skeleton loading={loading} /> : 
+           <div className="text-left">
+            <div className="flex justify-between items-center">
+              <Text className="uppercase text-md text-left mt-md leading-normal block font-semibold mb-sm">
+                { product_name }
+              </Text>
+              <div>
+              <Button onClick={handleButtonClick}>
+                  UPDATE
+                </Button>
+                <Button danger  className="mx-2"
+                  onClick={handleDelete}
+                >
+                  DELETE
+                </Button>
+              </div>
+            </div>
+              <Text className="uppercase font-md text-sm text-left mt-md leading-normal">
+                { description }
+              </Text>
+            </div>
+        }
+
+        
+        <Text className="font-bold text-lg text-left mt-sm">{loading ? '' : `P ${price}`}</Text>
         <div className="w-full flex flex-col gap-4 mt-xl justify-between">
           <div className="flex justify-between">
             <Text className="uppercase text-xs font-semibold">
@@ -110,6 +183,78 @@ const Product = () => {
           </div>
         </div>
       </div>
+      <Modal title='Update Product' 
+        open={isFormOpen}
+        closable
+        onCancel={() => setIsFormOpen(false)}
+        footer={false}
+        >
+          <Form
+          name="productForm"
+          onFinish={onFinish}
+          initialValues={{
+            productName: product_name,
+            descriptionField: description,
+            priceField: price,
+          }}
+          >
+          <Form.Item
+            label="Product Name"
+            name="productName"
+            rules={[{ required: true, message: 'Please input the product name.' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Description"
+            name="descriptionField"
+            rules={[{ required: true, message: 'Please input the description.' }]}
+          >
+            <Input.TextArea />
+          </Form.Item>
+
+          <Form.Item
+            label="Price"
+            name="priceField"
+            rules={[{ required: true, message: 'Please input the price.' }]}
+          >
+            <Input type="number" min={0} step={0.01} />
+          </Form.Item>
+          <Form.Item>
+            <div className="flex gap-2">
+              <Button type="secondary" className="text-black border-solid border border-gray-200 hover:bg-white"
+                  onClick={() => setIsFormOpen(false)}>
+                Cancel
+              </Button>
+              <Button loading={loading} 
+                  type="primary" htmlType="submit" className="text-black border-solid border border-gray-600 hover:bg-white">
+                Update
+              </Button>
+            </div>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal
+        title="Confirmation"
+        open={isDeleteOpen}
+        closable
+        footer={false}
+        destroyOnClose
+        onCancel={() => setIsDeleteOpen(false)}
+      >
+        <p>Are you sure you want to delete?</p>
+        <div className="flex gap-2">
+              <Button type="secondary" className="text-black border-solid border border-gray-200 hover:bg-white"
+                  onClick={() => setIsDeleteOpen(false)}>
+                Cancel
+              </Button>
+              <Button loading={loading} 
+                  type="primary" htmlType="submit" onClick={onDelete} className="text-black border-solid border border-gray-600 hover:bg-white">
+                Ok
+              </Button>
+            </div>
+      </Modal>
     </div>
   );
 };
